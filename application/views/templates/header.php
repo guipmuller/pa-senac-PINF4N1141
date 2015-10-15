6627d6b7ddf214dc54c80ee9543a4521ed6d1789
guipmuller <guilhermepmuller@gmail.com>
2015-10-14 21:30:32 -0300

    Iniciando

---
+++ application/views/templates/header.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <link href="../../../assets/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="../../../assets/css/styles.css" rel="stylesheet" type="text/css"/>
+        <title>SaciShop</title>
+
+    </head>
+    <body>
+
+        <h1><?php echo $title; ?></h1>
